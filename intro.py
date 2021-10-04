76cd71f35484df46c3c7ebf30a2e1f8ba8ef51cc
Codexgrey <thecodexgrey@gmail.com>
2021-10-04 15:02:11 +0100

    added a new file

---
+++ intro.py
@@ -0,0 +1,84 @@
+new_black = 10;
+
+# print(new_black);
+# print(type(new_black));
+
+# float_me = 0.5;
+# print(float_me);
+# print(type(float_me));
+
+# stringify = "We had a downpour 2 days ago in lagos!";
+# print(stringify);
+# print(type(stringify));
+
+# mylist = ['merovingian', "oracle", 0.25, True, 2500000]
+# print(mylist);
+# print(type(mylist));
+
+# mytuple = ('merovingian', "oracle", 0.25, True, 2500000)
+# print(mytuple);
+# print(type(mytuple));
+
+# sports = {"football", "cricket", "baseball", "cricket", "Badminton"}
+# print(sports);
+# print(type(sports));
+
+
+# firstNum = 67
+# secondNum = 40;
+
+# feedback = ((firstNum % 2 != 0) or (secondNum > firstNum)) and not((secondNum == 40) or (pssecondNum // 2 != 0))
+# print(feedback)
+
+# career = 'She is a model!'
+# output = 'mo' in career;
+# print(output)
+# print(f"I think {career}")
+
+# career = 'She is a model, lives in cali'
+# desired_char = career[-1]
+# desired_portion = career[:10]
+# desired_portion1 = career[4:]
+
+# print(desired_char)
+# print(desired_portion)
+# print(desired_portion1);
+
+# age = 24
+# name = "Blue"
+# career_new = f"{career} and is {age} years old!";
+
+# print(career_new)
+# print(career_new.title())
+# print(career_new.startswith("She is"))
+# print(career_new.index("cali"))
+# print(career_new.find("cali"))
+# print(career_new.split("!"))
+# print(type((career_new.split(" "))))
+# print(name.replace('ue', 'iss'));
+
+random_stuff = ["pawn", "phone", "pen", "ball", False, 99.69, 2021, 4+2j, "phone"]
+desired_items = random_stuff[::3]
+print(desired_items);
+
+random_stuff[3:6:2] = ["chair", True]
+print(random_stuff.count("phone"));
+print(random_stuff[0].replace("w", ""))
+
+flist = [3, 6, 7]
+slist = [8, 9, 10]
+flist.extend(slist)
+whole = flist + slist
+
+whole.sort(reverse=True)
+print(whole);
+
+mint = {'flavour': 'fresh', 'germs': 'dead'}
+print(type(mint))
+
+
+
+
+
+
+
